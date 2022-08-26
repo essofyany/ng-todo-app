00544c527a081df9e25f1cbdbc033b2ab1a7e04b
essofyany <soufyani.bilal@gmail.com>
2022-08-26 20:06:46 +0100

    create aside component

--- src/app/components/aside/aside.component.ts
+++ src/app/components/aside/aside.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-aside',
   templateUrl: './aside.component.html',
-  styleUrls: ['./aside.component.css']
+  styleUrls: ['./aside.component.css'],
 })
 export class AsideComponent implements OnInit {
+  topics = ['all', 'design', 'finance', 'client work'];
+  activeTopic = 'all';
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  setActiveTopic(topic: string) {
+    this.activeTopic = topic;
   }
-
 }
